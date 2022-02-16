fc403de1a7b4d09313578e57965eccb703cf6563
TremaineJScott <j.scott.tremaine@gmail.com>
2022-02-16 17:33:26 -0500

    Add project files.

---
+++ MockAssessment7/Controllers/DonutController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MockAssessment7.Models;
+using Flurl.Http;
+
+namespace MockAssessment7.Controllers
+{
+
+    public class DonutController : Controller
+    {
+        private List<Donut> _donuts;
+        private readonly string _apiBase = "https://grandcircusco.github.io/demo-apis";
+
+        public DonutController()
+        {
+            _donuts = new List<Donut>();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(int id)
+        {
+            string apiUri = $"{_apiBase}/donuts/{id}.json";
+            var apiTask = apiUri.GetJsonAsync<Donut>();
+            apiTask.Wait();
+
+            Donut donut = apiTask.Result;
+
+            return View("Details",donut);
+        }
+        // GET: DonutController
+        public ActionResult Index()        
+        {           
+            return View(_donuts);
+        }
+
+        // GET: DonutController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DonutController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DonutController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DonutController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DonutController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DonutController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DonutController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MockAssessment7/Models/Donut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockAssessment7.Models
+{
+    public class Donut
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int calories { get; set; }
+        public string photo { get; set; }
+        public List<string> extras { get; set; }
+    }
+}
